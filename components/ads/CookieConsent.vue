9367713a0c7731fe25265d357916abf79bc3f8c2
Kayra Uylar <k.uylar@outlook.com>
2022-11-05 18:51:50 +0300

    Remove the cookie consent

--- components/ads/CookieConsent.vue
+++ components/ads/CookieConsent.vue
@@ -1,25 +1,4 @@
 <template>
-  <div>
-    <div
-      ref="container"
-      class="container"
-      :class="{ 'mobile-menu-open': mobileMenuOpen }"
-      :style="{
-        visibility: shown ? 'visible' : 'hidden',
-      }"
-    >
-      <div class="card banner">
-        <span>
-          Modrinth uses cookies for various purposes. We encourage you to review
-          your privacy settings by clicking on the button below:
-        </span>
-        <div class="actions">
-          <button class="btn button" @click="review">Review</button>
-          <button class="btn brand-button" @click="hide">Accept all</button>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -72,69 +51,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.container {
-  width: 100%;
-  text-align: center;
-
-  z-index: 2;
-  position: fixed;
-  right: 0;
-  bottom: 0;
-
-  .banner {
-    font-size: 1.05rem;
-    border-radius: 0;
-    margin-bottom: 0;
-    box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.3);
-    padding: 1rem 1rem calc(var(--size-mobile-navbar-height) + 1rem);
-    transition: padding-bottom 0.25s ease-in-out;
-  }
-  .actions {
-    display: flex;
-    flex-direction: row;
-    margin-top: 1rem;
-    justify-content: center;
-
-    .btn {
-      margin-right: 0.5rem;
-    }
-  }
-
-  .banner {
-    margin-bottom: 0;
-  }
-
-  &.mobile-menu-open {
-    .banner {
-      padding-bottom: calc(var(--size-mobile-navbar-height-expanded) + 1rem);
-    }
-  }
-
-  @media screen and (min-width: 750px) {
-    .banner {
-      padding-bottom: 1rem;
-    }
-
-    &.mobile-menu-open {
-      bottom: 0;
-    }
-  }
-
-  @media screen and (min-width: 1024px) {
-    width: unset;
-    text-align: unset;
-
-    .banner {
-      border-radius: var(--size-rounded-card);
-      width: 18vw;
-      min-width: 16rem;
-      border-left: solid 5px var(--color-brand);
-      margin: 0 2rem 2rem 0;
-    }
-
-    .actions {
-      justify-content: unset;
-    }
-  }
-}
 </style>
